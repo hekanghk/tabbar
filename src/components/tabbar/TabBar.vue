7d4563c98ac25bbfa04c260f9d8e575c91aa9f74
mac <mac@nuc8.local>
2020-06-10 00:11:01 +0800

    第一次提交

---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBar.vue"
+  }
+</script>
+
+<style scoped>
+  @import "../../assets/css/base.css";
+
+  #tab-bar{
+    display: flex;
+    background-color: #f6f6f6;    /*添加背景灰*/
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;     /*这四行让tabbar到页面底部*/
+  }
+
+</style>
